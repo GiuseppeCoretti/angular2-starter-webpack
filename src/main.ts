12e16cc87ab7750b1edcdd77888e9a6b68416e53
Giuseppe Coretti <giucore@gmail.com>
2016-07-16 15:10:18 +0200

    Prima versione

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import 'core-js';
+import 'reflect-metadata';
+import 'zone.js/dist/zone';
+
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app/app.component';
+
+bootstrap(AppComponent);
